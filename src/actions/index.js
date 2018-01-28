11f4df4092a07581a0399b53d88b4d0e3baffddb
Khiem <khiemduy90@gmail.com>
2018-01-27 18:00:00 -0600

    added google map API

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,13 +1,14 @@
 import axios from 'axios';
 
 const API_KEY = '09ad610674f1b2ae6a74ae6997f42b8e';
-const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?&units=imperial&appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city) {
     const url = `${ROOT_URL}&q=${city}, US`;
     const request = axios.get(url);
+
     
     return {
         type: FETCH_WEATHER,
